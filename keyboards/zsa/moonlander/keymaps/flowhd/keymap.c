e5acb2ff3092a18d300a7350dd884795e8d6ad32
cabbott008 <curtisabbott@me.com>
2025-03-04 19:28:18 -0600

    added cmpl

--- keyboards/zsa/moonlander/keymaps/flowhd/keymap.c
+++ keyboards/zsa/moonlander/keymaps/flowhd/keymap.c
@@ -8,6 +8,7 @@ enum custom_keycodes {
     NDASH,
     DVIDE,
     DGREE,
+    CMPL,
     FLSH,
     R_QUE,
     R_EXC,
@@ -39,7 +40,7 @@ const uint16_t PROGMEM keymaps[] [MATRIX_ROWS] [MATRIX_COLS] = {
   ),
 
   [1] = LAYOUT_moonlander(
-    KC_F11,             KC_F9,              KC_F5,              KC_F1,              KC_F3,              KC_F7,             KC_NO,               FLSH,                KC_F6,              KC_F2,              KC_F10,             KC_F4,              KC_F8,              KC_F12,
+    KC_F11,             KC_F9,              KC_F5,              KC_F1,              KC_F3,              KC_F7,             CMPL,                FLSH,                KC_F6,              KC_F2,              KC_F10,             KC_F4,              KC_F8,              KC_F12,
     KC_TAB,             KC_NO,              KC_NO,              KC_NO,              KC_NO,              KC_NO,             KC_NO,               KC_NO,               DVIDE,              LSFT(KC_7),         LSFT(KC_8),         KC_LPRN,            KC_RPRN,            KC_PIPE,
     KC_ESC,             SLCTALL,            UNDO,               CUT,                COPY,               PASTE,             KC_NO,               KC_NO,               KC_EQL,             LSFT(KC_4),         LSFT(KC_5),         LSFT(KC_6),         KC_LBRC,            KC_RBRC,
     KC_LSFT,            KC_NO,              KC_NO,              KC_NO,              KC_NO,              KC_NO,                                                       KC_GRV,             LSFT(KC_1),         LSFT(KC_2),         LSFT(KC_3),         LSFT(KC_LBRC),      LSFT(KC_RBRC),
@@ -130,6 +131,10 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       case FLSH:
       if (record->event.pressed) {
           SEND_STRING ("qmk flash -kb moonlander -km flowhd");
+      }
+      case CMPL:
+      if (record->event.pressed) {
+          SEND_STRING ("qmk compile -kb moonlander -km flowhd");
       }
 
       break;
